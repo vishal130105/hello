166fdf70538dce4fe842a810a3fa6dfd9aca34bf
vishal130105 <71890916+vishal130105@users.noreply.github.com>
2020-09-30 19:50:34 +0530

    Update Addition.py

--- Addition.py
+++ Addition.py
@@ -1,11 +1,11 @@
 print("Hello there!")
 print("Welcome To Addition")
-prnt("First number")
+print("First number")
 num_1 = int(input(""))
 print("Second number")
 num_2 = int(input(""))
 
-Sun = str(num1 + num_2)
+Sum = str(num_1 + num_2)
 
 print("The sum is " + Sum)
 
