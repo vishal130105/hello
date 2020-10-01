3d06fe7e208c08a553cf704a2f6ea4cd027d03b3
Gauravsaha-97 <gauravsaha385@gmail.com>
2020-10-01 12:35:53 +0530

    Updated the code filesand some typos in 'READ.md'

--- Addition.py
+++ Addition.py
@@ -3,28 +3,19 @@ print("Hello there!")
 name = str(input("Name: ")) 
 print("Welcome To Addition")
 
-
-
-print("Welcome To Addition Only ")
-
-
-print("Welcome To Addition")
-
-
-print("First number")
+print("First number: ")
 num_1 = int(input(""))
-print("Second number")
+
+print("Second number: ")
 num_2 = int(input(""))
 
 Sum = str(num_1 + num_2)
 
 
-Sum = str(num_1 + num_2)
-
 
 print("The sum is " + Sum)
 print("")
 print("")
-print("Thanks " + name " for using my program")
+print("Thanks " + name + " for using my program")
 
 
--- Division.py
+++ Division.py
@@ -1,12 +1,17 @@
 print("Hello People! ")
+name = str(input("Name: "))
 print("Only Division Calculator Welcomes you")
+
 print("1st number")
 num_1 = int(input(""))
+
 print("2nd number")  
 num_2 = int(input(""))
 
-Quotient = str(num_1 / num_2)
-
-
+Quotient = str(num_1 // num_2)
+remainder = str(num_1 % num_2)
 
 print("The quotient is " + Quotient)
+print("The remainder is " + remainder)
+
+print("Thanks " + name + " for using my program")
