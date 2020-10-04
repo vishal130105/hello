f68cd0cf4bf85bf524995867b48b67510677a8f1
Saurav Kumar <saurav0001kumar@gmail.com>
2020-10-04 19:32:38 +0530

    GCD/HCF added

---
+++ GCD-or-HCF.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+"""
+Greatest Common Divisor(GCD) OR Highest Common Factor(HCF)
+
+@author: SK
+"""
+#GCD / HCF
+
+a=int(input("1st No.: "))
+b=int(input("2nd No.: "))
+num1=a
+num2=b
+while(b!=0):
+    t=a%b
+    a=b
+    b=t
+print("\nGCD/HCF of",num1,"&",num2,"is",a)
