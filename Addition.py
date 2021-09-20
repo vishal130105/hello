675d4ea98b768a92f3cd669c7f55dbc18934aff9
19-ade <f20190518@hyderabad.bits-pilani.ac.in>
2021-09-20 22:46:43 +0530

    Removed Syntax error

--- Addition.py
+++ Addition.py
@@ -2,29 +2,17 @@
 print("Hello there!")
 name = str(input("Name: ")) 
 print("Welcome To Addition")
-
-
-
 print("Welcome To Addition Only ")
-
-
-print("Welcome To Addition")
-
-
 print("First number")
 num_1 = int(input(""))
+
 print("Second number")
 num_2 = int(input(""))
-
 Sum = str(num_1 + num_2)
-
-
-Sum = str(num_1 + num_2)
-
 
 print("The sum is " + Sum)
 print("")
 print("")
-print("Thanks " + name " for using my program")
+print("Thanks " + name + " for using my program")
 
 
