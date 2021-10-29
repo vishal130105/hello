460ae16628b4f31682fc9fd04f8038ff4ab23d1a
vishal130105 <71890916+vishal130105@users.noreply.github.com>
2021-10-29 21:42:34 +0530

    Revert "Update Multiplicaton.py"

--- Multiplicaton.py
+++ Multiplicaton.py
@@ -5,9 +5,11 @@ print("First number")
 Num_1st = int(input(""))
 print("Second number")
 Num_2nd = int(input(""))
+
 Product = str(Num_1st* Num_2nd)
 print("The product is " + Product)
 print("")
+
 print("")
+
 print("Thanks " + name +"for using my program")
-print(":)")
