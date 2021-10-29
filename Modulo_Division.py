a266e361278a4f3805c168fd13727de2916abfe7
vishal130105 <71890916+vishal130105@users.noreply.github.com>
2021-10-29 21:33:09 +0530

    Revert "Update Modulo_Division.py"

--- Modulo_Division.py
+++ Modulo_Division.py
@@ -3,9 +3,12 @@ name=input("Enter your name: ")
 print("\nWelcome",name,"to Modulo Division in Python.\n#Modulo Divison just gives the remainder when one number divides another.")
 print("\n*For Example: 15 mod 2 =",15%2)
 print(" In this example, 15 is divided by 2 and leaves",15%2,"as remainder.")
+
 num1 = int(input("\nEnter 1st Number: "))
+
 num2 = int(input("\nEnter 2nd Number: "))
+
 result = num1 % num2
+
 print("\n",num1,"is divided by",num2,"and leaves",result,"as remainder.")
 print("\n\nThanks",name,"for using my program!")
-print("Thanks once again")
